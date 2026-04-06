72bf4d79f20462971dfbd5972aa54140d874c4b4
Santosh Kumar Janapala <santosh_janapala@hotmail.com>
2026-04-05 22:06:35 -0400

    initial core app

---
+++ Models/Appsettings.cs
@@ -0,0 +1,8 @@
+﻿namespace demoproject.Models
+{
+    public static class Appsettings
+    {
+        public static string AppSettingEnv { get; set; }
+        public static string Environment { get; set; }
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# demoproject
-demo project 
